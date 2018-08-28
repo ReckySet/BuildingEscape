e4a389c437b8e721acec3cd4c3b70ad7464f1a4b
ReckySet <kiseimatsumoto@gmail.com>
2018-08-29 04:11:34 +0900

    08 Before rotate the door

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
+#include "Runtime/Engine/Classes/GameFramework/Actor.h"
+#include "Runtime/Core/Public/Math/Vector.h"
 
 
 // Sets default values for this component's properties
@@ -10,7 +13,6 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -19,7 +21,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at (%s)"),*ObjectName,*ObjectPos);
 	
 }
 
