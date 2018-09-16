447cf7587a981d44533f72db2e8da3f0aee39a98
ReckySet <kiseimatsumoto@gmail.com>
2018-09-16 13:42:56 +0900

    14 trigger with weight

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -43,6 +43,9 @@ private:
 
 	float LastDoorOpenTime;
 
-	AActor* ActorThatOpens; // Remember pawn inherits from actor
 	AActor* Owner;
+
+	// Returns total mass in kg
+	float GetTotalMassOfActorsOnPlate();
+
 };
